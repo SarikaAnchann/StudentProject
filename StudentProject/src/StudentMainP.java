3c634292b5cd01f8f52160dac46a5fbd7741055b
HP <HP@LAPTOP-NTAL9CM3>
2022-08-31 15:03:14 +0530

    first commit

---
+++ StudentProject/src/StudentMainP.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class StudentMainP{
+
+	public static void main(String[] args) {
+		
+		StudentP[] student= new StudentP[100];
+		Map <Integer, Student> sDetails=new HashMap <Integer, Student>();
+				 while(true) {
+				 System.out.println("Hello! Let us know what would you like to do");
+				 System.out.println("Enter 1 to see all the details, 2 to add, 3 to update and 4 to delete");
+		Scanner scan=new Scanner(System.in);
+		int option=scan.nextInt();
+			 switch (option) {
+			 case 1:{
+		System.out.println(sDetails);
+		break;
+			}
+			 case 2:{
+				 System.out.println("please enter the number of students to be added");
+				 int num=scan.nextInt();
+for(int i=1; i<=num;i++) {
+	student[i]= new StudentP();
+}
+for(int i=1;i<=num;i++) {
+				 System.out.println("please enter the id of student "+i);
+				 student[i].setId(scan.nextInt());
+				 System.out.println("please enter the name of student "+i);
+				 student[i].setName(scan.next());
+				 System.out.println("please enter the age of student "+i);
+				 student[i].setAge(scan.nextInt());
+				 System.out.println(student[i]);
+				 sDetails.putAll((Map<? extends Integer, ? extends Student>) student[i]);
+//				 Object[] s=sDetails.entrySet().toArray();                 
+//				 System.out.println(Arrays.toString(s));
+}  
+System.out.println(sDetails);
+
+		break;
+			 }
+			 
+	}
+		}
+	}}
+
+	
---
+++ StudentProject/src/StudentP.java
@@ -0,0 +1,27 @@
+
+public class StudentP {
+ private int age;
+  private String name;
+private int id;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String toString() {
+	return "name:"+name+", age:" +age+", ID:"+id;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
